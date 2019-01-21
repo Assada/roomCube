6fe7374e0725b19bf3eddaeb62084ec539ae21d7
assada <assada.ua@gmail.com>
2019-01-22 00:46:18 +0200

    ...

--- include/cube_esp.h
+++ include/cube_esp.h
@@ -1,5 +1,5 @@
 #include <ESP8266WiFi.h>
-// #include <ESP8266WebServer.h>
+#include <ESP8266WebServer.h>
 #include <TimeLib.h>
 #include <Wire.h>
 #include <SPI.h>
@@ -14,7 +14,7 @@
 
 int last_conn_status;
 
-// ESP8266WebServer server(80);
+ESP8266WebServer server(80);
 WiFiUDP udp;
 EasyNTPClient ntpClient(udp, "pool.ntp.org");
 
